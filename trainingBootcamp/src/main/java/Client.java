7390f59aa8603d61caf5375681385bb3118547c0
werewolfLuis <luis.js.py@gmail.com>
2022-11-06 17:30:27 -0400

    Agregando desde INtellij

---
+++ trainingBootcamp/src/main/java/Client.java
@@ -0,0 +1,15 @@
+public class Client {
+    public static void main(String [] andse){
+        int a=8,b=2;
+        if (a > b){
+            a=a*2;
+            System.out.println(a);
+        }else
+        {
+            b=b*2;
+            System.out.println(b);
+        }
+
+    }
+
+}
